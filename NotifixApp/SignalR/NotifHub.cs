2ed9a8f49f2dba9b7a89061ae18be4af410a2b4a
potemkin-git <bennourine.ch@hotmail.fr>
2018-03-27 08:58:38 +0200

    signalr ze beginning

---
+++ NotifixApp/SignalR/NotifHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+namespace SignalRNotifier
+{
+    public class NotifHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            Clients.All.addNewMessageToPage(name, message);
+        }
+    }
+}
